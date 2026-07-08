115f05e87382a53ba4f8139efbdcbd298bdc553e
Luis Ortiz <luis.ortiz@gorillalogic.com>
2026-07-08 08:59:20 -0600

    inital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.example_pipe import pipe_main
+
+
+def main():
+    pipe_main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/example_pipe.py
@@ -0,0 +1,22 @@
+from multiprocessing import Process, Pipe
+
+
+def f(conn):
+    # sends data to the reciever
+    conn.send([42, None, "Hello"])
+    conn.send([84, None, "There"])
+    # once the connection closes, we cannot send more data
+    conn.close()
+
+
+def pipe_main():
+    parent_conn, child_conn = Pipe()
+    process = Process(target=f, args=(child_conn,))
+    process.start()
+
+    # we must have a receive method for each send that is executed
+    print(parent_conn.recv())
+    print(parent_conn.recv())
+
+    # waits until the child process finishes
+    process.join()
